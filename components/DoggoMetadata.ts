4020c0a2eeb3b66c356fefa3f92c928b3f794e31
Paul Fannon <paul.fannon@wwt.com>
2024-02-08 13:59:12 -0600

    Define metadata component and import into layout

---
+++ components/DoggoMetadata.ts
@@ -0,0 +1,7 @@
+'use client'
+import type { Metadata } from 'next'
+
+export const DoggoMetadata: Metadata = {
+    title: 'Doggo',
+    description: 'Application for viewing doggos'
+}
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,3 +1,5 @@
+import { DoggoMetadata } from './DoggoMetadata'
+
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <main>{children}</main>
