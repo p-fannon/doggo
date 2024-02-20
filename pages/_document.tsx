fd47e88f20d109e20e46c8116630abf5707a0f68
Paul Fannon <paul.fannon@wwt.com>
2024-02-20 16:12:53 -0600

    Add favicon and apple touch icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,8 @@ export default function Document() {
     <Html lang='en'>
       <Head>
         <link rel='manifest' href='/manifest.webmanifest' />
-        <link rel='apple-touch-icon' href='/icon-192x192.png' />
+        <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
+        <link rel='shortcut icon' href='/favicon.ico' />
         <meta name='theme-color' content='#ffccdc' />
       </Head>
       <body>
