190b10bf8f71fda37813ba8797926e47c75d3189
Paul Fannon <paul.fannon@wwt.com>
2024-02-20 16:03:40 -0600

    Add metadata and public directory

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,22 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Doggo App',
+    description: 'Application for humans to view doggos',
+    applicationName: 'Doggo',
+    robots: {
+        index: false,
+        follow: false,
+        nocache: true,
+        googleBot: {
+            index: false,
+            follow: false,
+            noimageindex: true
+        },
+    }
+}
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
  
 export default function Document() {
   return (
-    <Html>
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <link rel='manifest' href='/manifest.webmanifest' />
+        <link rel='apple-touch-icon' href='/icon-192x192.png' />
+        <meta name='theme-color' content='#ffccdc' />
+      </Head>
       <body>
         <Main />
         <NextScript />
